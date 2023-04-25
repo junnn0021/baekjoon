98d18987ed44b7ea93dd84f58ca1b930be16c219
junnn0021 <junnn0021@gmail.com>
2023-04-25 21:43:01 +0900

    practice

---
+++ level 5/9086-strings.js
@@ -0,0 +1,19 @@
+// 문제
+// 문자열을 입력으로 주면 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 입력의 첫 줄에는 테스트 케이스의 개수 T(1 ≤ T ≤ 10)가 주어진다. 각 테스트 케이스는 한 줄에 하나의 문자열이 주어진다. 문자열은 알파벳 A~Z 대문자로 이루어지며 알파벳 사이에 공백은 없으며 문자열의 길이는 1000보다 작다.
+
+// 출력
+// 각 테스트 케이스에 대해서 주어진 문자열의 첫 글자와 마지막 글자를 연속하여 출력한다.
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const testCaseCount = Number(input[0]);
+
+for (let i = 1; i <= testCaseCount; i++) {
+  const word = input[i];
+  const firstChar = word.charAt(0);
+  const lastChar = word.charAt(word.length - 1);
+  console.log(`${firstChar}${lastChar}`);
+}
